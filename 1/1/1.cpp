721fd2f3e23e3b8075e4c1a23d27c3d072e0e5e5
Ш.М.Р. <s.m.r.00800ist.@gmail.com>
2024-06-26 21:28:25 +0300

    Initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include<string>
+#include<map>
+#include<algorithm>
+
+int main()
+{
+	std::string str = "Hello world!!";
+	std::cout << "[IN]: ";
+	std::cout << str;
+	std::map<char, int> map = {};
+
+	int num = 0;
+	for (int i = 0; i < str.size(); i++) {
+		num = 0;
+		for (int j = 0; j < str.size(); j++) {
+			if (str[i] == str[j]) {
+				num++;
+			}
+			else {
+				continue;
+			}
+		}
+		map[str[i]] = num;
+	}
+
+	for (const auto& pair : map) {
+		std::cout << "\n" << pair.first << ": " << pair.second;
+	}
+}
