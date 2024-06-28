e9226377d01ad0a7dfefd0a8ea8cbd43fa674646
Ш.М.Р. <s.m.r.00800ist.@gmail.com>
2024-06-28 22:45:37 +0300

    Initial commit

--- 1/1/1.cpp
+++ 1/1/1.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include<string>
-#include<map>
+#include<vector>
 #include<algorithm>
 
 int main()
@@ -8,7 +8,8 @@ int main()
 	std::string str = "Hello world!!";
 	std::cout << "[IN]: ";
 	std::cout << str;
-	std::map<char, int> map = {};
+	std::vector<char> str_vec = {};
+	std::vector<int> num_vec = {};
 
 	int num = 0;
 	for (int i = 0; i < str.size(); i++) {
@@ -21,10 +22,43 @@ int main()
 				continue;
 			}
 		}
-		map[str[i]] = num;
+		str_vec.push_back(str[i]);
+		num_vec.push_back(num);
 	}
 
-	for (const auto& pair : map) {
-		std::cout << "\n" << pair.first << ": " << pair.second;
+	for (int i = 0; i < str_vec.size(); i++) {
+		num = 0;
+		for (int j = 0; j < str_vec.size(); j++) {
+			if (str_vec[i] == str_vec[j]) {
+				num++;
+				if (num > 1) {
+					str_vec.erase(str_vec.begin() + j);
+					num_vec.erase(num_vec.begin() + j);
+				}
+				else {
+					continue;
+				}
+			}
+			else {
+				continue;
+			}
+		}
+	}
+
+	for (int i = 0; i < num_vec.size(); i++) {
+		for (int j = 0; j < num_vec.size(); j++) {
+			if (num_vec[i] > num_vec[j]) {
+				std::swap(num_vec[i], num_vec[j]);
+				std::swap(str_vec[i], str_vec[j]);
+			}
+			else {
+				continue;
+			}
+		}
+	}
+
+	std::cout << "\n[OUT]: \n";
+	for (int i = 0; i < str_vec.size(); i++) {
+		std::cout << str_vec[i] << ": " << num_vec[i] << "\n";
 	}
 }
---
+++ 2/2/2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <set>
+
+int main() {
+
+    std::cout << "[IN]: \n";
+    int total;
+    int num;
+    std::cin >> total;
+
+    std::set<int> set;
+    for (int i = 0; i < total; i++) {
+        std::cin >> num;
+        set.insert(num);
+    }
+
+    std::cout << "[OUT]: \n";
+    for (auto it = set.rbegin(); it != set.rend(); ++it) {
+        std::cout << *it << "\n";
+    }
+}
